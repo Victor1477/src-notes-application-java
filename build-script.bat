aa0dfa8a129c0be2016551496fd218125555d11f
Joao Oliveira <Joao.Oliveira7@br.ey.com>
2024-08-08 14:20:27 -0300

    Adding build-script

---
+++ build-script.bat
@@ -0,0 +1,4 @@
+set API_JWT_ALGORITHM_SECRET=notes-application-jwt-secret
+set API_SECURITY_LOGIN_USERNAME=victor
+set API_SECURITY_LOGIN_BCRYPT_PASSWORD=$2a$12$uxRyqtZ5VEe.H0raGm4aiekEjdctWZkXSpXO88czd1v/whhesBreG
+mvnw clean package
