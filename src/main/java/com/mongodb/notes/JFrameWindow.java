cb9afa4b5e60cc2f142448607fc83fb782040a15
Joao Oliveira <victorjoao53@hotmail.com>
2024-08-23 19:46:03 -0300

    Jframe console

---
+++ src/main/java/com/mongodb/notes/JFrameWindow.java
@@ -0,0 +1,34 @@
+package com.mongodb.notes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class JFrameWindow extends OutputStream {
+
+    private JFrame jFrame;
+    private JScrollPane jScrollPane;
+    private JTextArea jTextArea;
+
+    public JFrameWindow() {
+        jFrame = new JFrame();
+        jTextArea = new JTextArea();
+        jScrollPane = new JScrollPane(jTextArea);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setBounds(400, 150, 1280, 768);
+        jTextArea.setBackground(Color.BLACK);
+        jTextArea.setForeground(Color.GREEN);
+        jTextArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 13));
+        jFrame.add(jScrollPane);
+        jFrame.setVisible(true);
+        jTextArea.setVisible(true);
+        jTextArea.setEditable(false);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        jTextArea.append(String.valueOf((char) b));
+        jTextArea.setCaretPosition(jTextArea.getDocument().getLength());
+    }
+}
--- src/main/java/com/mongodb/notes/NotesApplication.java
+++ src/main/java/com/mongodb/notes/NotesApplication.java
@@ -3,9 +3,12 @@ package com.mongodb.notes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.PrintStream;
+
 @SpringBootApplication
 public class NotesApplication {
     public static void main(String[] args) {
+        System.setOut(new PrintStream(new JFrameWindow()));
         SpringApplication.run(NotesApplication.class, args);
     }
 }
