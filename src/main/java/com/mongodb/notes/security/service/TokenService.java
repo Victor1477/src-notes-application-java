9c6b3b1b1a938c0707475c6c2c3ac590c139a4e1
João Victor Silva De Oliveira <joao.soliveira@telefonicati.onmicrosoft.com>
2026-08-20 19:09:40 -0300

    Improvement

--- src/main/java/com/mongodb/notes/security/service/TokenService.java
+++ src/main/java/com/mongodb/notes/security/service/TokenService.java
@@ -10,15 +10,17 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.util.Random;
 
 @Service
 public class TokenService {
+    private static String SECRET_SUBJECT;
 
     @Value("${api.jwt.algorithm.secret}")
     private String secret;
 
     public String generateToken(UserDetails user) {
-        Algorithm algorithm = Algorithm.HMAC256(secret);
+        Algorithm algorithm = Algorithm.HMAC256(secret + getSecretSubject());
         return JWT.create().withIssuer("notes-application")
                 .withSubject(user.getUsername())
                 .withExpiresAt(generateExpirationDate())
@@ -26,12 +28,28 @@ public class TokenService {
     }
 
     public String validateToken(String token) {
-        Algorithm algorithm = Algorithm.HMAC256(secret);
+        Algorithm algorithm = Algorithm.HMAC256(secret + getSecretSubject());
         return JWT.require(algorithm)
                 .withIssuer("notes-application")
                 .build()
                 .verify(token)
                 .getSubject();
+    }
+
+    private static String getSecretSubject() {
+        if (SECRET_SUBJECT == null) {
+            SECRET_SUBJECT = generateRandomString();
+        }
+        return SECRET_SUBJECT;
+    }
+
+    private static String generateRandomString() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 2048; i++) {
+            sb.append((char) random.nextInt(97, 123));
+        }
+        return sb.toString();
     }
 
     public Instant generateExpirationDate() {
